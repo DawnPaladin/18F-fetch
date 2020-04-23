9ccc6092095a8fde6f6dce7d502125360e3a9beb
James Harris <computersherpa@gmail.com>
2020-04-23 16:22:38 -0500

    Return fetch() promise instead of creating a new one

--- api/managed-records.js
+++ api/managed-records.js
@@ -22,45 +22,41 @@ function retrieve(options = {}) {
 		url = URI(url).addSearch("color[]", color)
 	});
 	
-	const promise = new Promise((resolve, reject) => {
-		// fetch data from API
-		fetch(url).then(response => {
-			return response.json();
-		}).then(data => {
-			
-			// Determine whether there's another page of results
-			const nextPage = data.splice(10);
-			const nextPageNum = nextPage.length > 0 ? pageNum + 1 : null;
-			
-			// Initialize output
-			const output = {
-				ids: [],
-				open: [],
-				closedPrimaryCount: 0,
-				previousPage: pageNum === 1 ? null : pageNum - 1,
-				nextPage: nextPageNum,
-			};
-			
-			// Process records
-			const primaryColors = ["red", "blue", "yellow"];
-			data.forEach(row => {
-				output.ids.push(row.id);
-				row.isPrimary = primaryColors.includes(row.color);
-				if (row.disposition === "open") {
-					output.open.push(row);
-				}
-				if (row.disposition === "closed" && row.isPrimary) {
-					output.closedPrimaryCount += 1;
-				}
-			})
-			
-			resolve(output);
-		}).catch(error => {
-			console.log("Network error:", error);
-		});
+	// fetch data from API
+	return fetch(url).then(response => {
+		return response.json();
+	}).then(data => {
+		
+		// Determine whether there's another page of results
+		const nextPage = data.splice(10);
+		const nextPageNum = nextPage.length > 0 ? pageNum + 1 : null;
+		
+		// Initialize output
+		const output = {
+			ids: [],
+			open: [],
+			closedPrimaryCount: 0,
+			previousPage: pageNum === 1 ? null : pageNum - 1,
+			nextPage: nextPageNum,
+		};
+		
+		// Process records
+		const primaryColors = ["red", "blue", "yellow"];
+		data.forEach(row => {
+			output.ids.push(row.id);
+			row.isPrimary = primaryColors.includes(row.color);
+			if (row.disposition === "open") {
+				output.open.push(row);
+			}
+			if (row.disposition === "closed" && row.isPrimary) {
+				output.closedPrimaryCount += 1;
+			}
+		})
+		
+		return output;
+	}).catch(error => {
+		console.log("Network error:", error);
 	});
-	
-	return promise;
 }
 
 export default retrieve;
