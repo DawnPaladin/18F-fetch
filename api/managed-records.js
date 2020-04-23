935de27e29519596ce25984596003d393c37ab5a
James Harris <computersherpa@gmail.com>
2020-04-23 15:45:10 -0500

    Determine whether another page of results is available after this one

--- api/managed-records.js
+++ api/managed-records.js
@@ -10,10 +10,12 @@ function retrieve(options = {}) {
 	if (!options.page) options.page = 1;
 	if (!options.colors) options.colors = [];
 	
+	const pageNum = options.page;
+	
 	// build query URL
-	const pageLength = 10;
-	const offset = (options.page - 1) * pageLength;
-	let url = URI(window.path).addSearch("limit", pageLength).addSearch("offset", offset);
+	const offset = (pageNum - 1) * 10;
+	// Page length is 10, but we need to know if there's a next page, so we try to retrieve 11 at a time.
+	let url = URI(window.path).addSearch("limit", 11).addSearch("offset", offset);
 	
 	// add colors
 	options.colors.forEach(color => {
@@ -24,15 +26,31 @@ function retrieve(options = {}) {
 	fetch(url).then(response => {
 		return response.json();
 	}).then(data => {
-		console.log(data);
+		
+		// Determine whether there's another page of results
+		const nextPage = data.splice(10);
+		const nextPageNum = nextPage.length > 0 ? pageNum + 1 : null;
+		
+		console.log(pageNum, data, nextPageNum);
+		
+		const output = {
+			ids: [],
+			open: [],
+			closedPrimaryCount: 0,
+			previousPage: pageNum === 1 ? null : pageNum - 1,
+			nextPage: nextPageNum,
+		}
 	}).catch(error => {
 		console.log("Network error:", error);
 	})
+	
 	const promise = new Promise((resolve, reject) => {
 		resolve()
 	})
 	return promise;
 }
-retrieve({page: 2, colors: ["red", "green"]});
+retrieve({page: 10, colors: ["red"]});
+retrieve({page: 11, colors: ["red"]});
+retrieve({page: 12, colors: ["red"]});
 
 export default retrieve;
